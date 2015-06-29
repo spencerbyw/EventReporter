688c5f17e70fa8a789e09f75bf96210d84b43c3d
Spencer A. Bywater <ntrwebmaster@gmail.com>
2015-06-29 14:03:09 -0700

    Update queue print
    Added functionality to print in sets of 10.

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -64,18 +64,36 @@ def print_row_headers
 end
 
 def print_queue(queue)
-	queue.each do |row|
-		printf "%-#{@column_lengths[:last_name] + 2}s", "#{row[:last_name]}"
-		printf "%-#{@column_lengths[:first_name] + 2}s", "#{row[:first_name]}"
-		printf "%-#{@column_lengths[:email] + 2}s", "#{row[:email_address]}"
-		printf "%-#{@column_lengths[:zipcode] + 2}s", "#{row[:zipcode]}"
-		printf "%-#{@column_lengths[:city] + 2}s", "#{row[:city]}"
-		printf "%-#{@column_lengths[:state] + 2}s", "#{row[:state]}"
-		printf "%-#{@column_lengths[:address] + 2}s", "#{row[:street]}"
-		printf "%-#{@column_lengths[:phone] + 2}s", "#{row[:homephone]}"
-		printf "\n" # puts "#{row[3]}\t\t#{row[2]}\t#{row[4]}\t#{row[-1]}\t#{row[-3]}\t#{row[-2]}\t#{row[-4]}\t#{row[-5]}"
+	total_records = queue.to_a.size
+	start = 0
+	start_to = 0
+	queue.each_slice(10) do |slice|
+		slice.each do |row|
+			printf "%-#{@column_lengths[:last_name] + 2}s", "#{row[:last_name]}"
+			printf "%-#{@column_lengths[:first_name] + 2}s", "#{row[:first_name]}"
+			printf "%-#{@column_lengths[:email] + 2}s", "#{row[:email_address]}"
+			printf "%-#{@column_lengths[:zipcode] + 2}s", "#{row[:zipcode]}"
+			printf "%-#{@column_lengths[:city] + 2}s", "#{row[:city]}"
+			printf "%-#{@column_lengths[:state] + 2}s", "#{row[:state]}"
+			printf "%-#{@column_lengths[:address] + 2}s", "#{row[:street]}"
+			printf "%-#{@column_lengths[:phone] + 2}s", "#{row[:homephone]}"
+			printf "\n" # puts "#{row[3]}\t\t#{row[2]}\t#{row[4]}\t#{row[-1]}\t#{row[-3]}\t#{row[-2]}\t#{row[-4]}\t#{row[-5]}"
+		end
+		# get correct "to" value
+		if (start + 10) < total_records
+			start_to = start + 10
+		else
+			start_to = total_records
+		end
+		puts "Displaying records #{start} - #{start_to} of #{total_records}"
+
+		if total_records > 10
+			puts "Press the space bar or enter key to show the next set of records..."
+			start += 10
+			gets.chomp
+		end
 	end
-	puts " "
+	# puts " "
 	
 end
 
